839cd593590621d5262eef31bda5b5b47a0a7f89
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-26 09:34:35 +0530

    ADDED: Welcome message

---
+++ src/main/java/com/hotelResevation/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.hotelResevation;
+
+public class HotelReservation {
+    public static void main(String args []){
+        System.out.println("Welcome to the Hotel Reservation System !!");
+    }
+}
